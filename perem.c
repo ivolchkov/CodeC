843dd405f111c7fa29c360e78aaec68ef25f9d57
ivolchkov <igorik32506632@gmail.com>
2017-11-05 19:27:58 +0200

    third commit

---
+++ perem.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+      int myVar = 50;
+      int number; //не инициализирована
+      number = 12;
+      int var,realtog,cool;
+      var = 758;
+      char letter = 'A';
+      int gigalul,numbers;
+      numbers = 35;
+      float decimal = 2.3565;
+      short number2 = 999;
+      double number3 = 0.6564845;
+      long number4 = 54649846216;
+
+}
