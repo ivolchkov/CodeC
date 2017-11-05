879d889ff7ab97aa434f07e21e04d8128758f122
ivolchkov <igorik32506632@gmail.com>
2017-11-05 18:47:37 +0200

    first commit

---
+++ my_firstprogram.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+        printf("my first program in C language!\n" );
+        printf("Моя первая программа на С\n" );
+        return 0;
+        //comments
+        /*long comments
+
+
+        */
+}
