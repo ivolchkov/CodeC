006174407680987be7adee421afe4bbfb6a9f2c6
ivolchkov <igorik32506632@gmail.com>
2017-11-07 21:05:31 +0200

    fifth commit

---
+++ escape.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("String will be continued \nhere\n");
+	printf("\t tabulation\n");
+	printf("This symbol A\bwill be destroyed\n");
+	printf("This \"word\"\n");
+	printf("also \\\n");
+	printf("percent %%\n");
+	printf("shout !\n");
+	printf("\r Cool story Bob\?\n");
+}
