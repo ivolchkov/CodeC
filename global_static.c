315b5d9b0c31ad0b06796c02ba488d65a52ea73c
ivolchkov <igorik32506632@gmail.com>
2017-11-14 21:11:07 +0200

    ninth commit

--- global_static.c
+++ global_static.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-static int number = 500;//Global local variable
+static int number = 500;//Global local variable;
 
 int main(void)
 {
-	extern int number;//Using global local variable
+	extern int number;//Using global local variable;
 	printf("number = %d\n",number );
-	extern int number2;//It will be placed in another file
+	extern int number2;//It will be placed in another file;
 	printf("number2 = %d\n",number2 );
 
 }
---
+++ test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	/*int two = 2;
+	float a = (double)two;
+
+	float decimal = 5.2;
+	int s = (int) decimal;
+	printf("%d\n",s );
+	//float a = 3 / (two+0.0);
+	printf("%1.1f\n",a ); */
+	int free = 3;
+	int two = 2;
+	int f = 4;
+	printf("%1.1f\n", two+0.0 );//to type int add type float 2+0.0=2.00
+	printf("%1.1f\n", (double)free);//yavnoe izmeneniye
+	int index = (f/(double)two);
+	printf("%d\n",index );
+	
+}
