c598455c493135497776af048a6cb9b03601ff1d
ivolchkov <igorik32506632@gmail.com>
2017-11-18 19:03:06 +0200

    twelwth commit

---
+++ const.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define AUTHOR "Igor"
+#define NUMBER 100
+#define LINE "_______"
+#define SYMBOL "$%^&@#$%"
+#define STRING "Programing in C easy"
+ int main ()
+ {
+ 	const int num =100;
+ 	const char z='z';
+ 	printf("const AUTHOR - %s\n", AUTHOR  );
+ 	printf("NUMBER - %d\n", NUMBER );
+ 	printf("Const LINE - %s\n",LINE );
+ 	printf("Const SYMBOL -%s\n", SYMBOL );
+ 	printf("Const num - %d\n",num );
+ 	printf("Symbol - %c\n",z );
+ 	printf("String - %s\n",STRING );
+
+
+
+
+
+ }
